fe94e944d1d62bafc96018c88deba44ff15420fd
ShwetaYadav19 <shwetuyadav19@gmail.com>
2020-03-08 22:38:39 +0530

    Added ‘Login’ button on Header

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Home from './screens/home/Home';
 
+
 ReactDOM.render(<Home/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
